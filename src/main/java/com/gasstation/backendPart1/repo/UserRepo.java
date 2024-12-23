a6a35b9290cdc3f0e6514509cf74e1ca0dd7c697
rashmika <rashmikanethsarani119@gmail.com>
2024-12-23 23:27:45 +0530

    created the basic CRUD opereations in UserServices class

--- src/main/java/com/gasstation/backendPart1/repo/UserRepo.java
+++ src/main/java/com/gasstation/backendPart1/repo/UserRepo.java
@@ -1,4 +1,7 @@
 package com.gasstation.backendPart1.repo;
 
-public interface UserRepo {
+import com.gasstation.backendPart1.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User,Integer> {
 }
--- src/main/java/com/gasstation/backendPart1/service/UserService.java
+++ src/main/java/com/gasstation/backendPart1/service/UserService.java
@@ -1,4 +1,50 @@
 package com.gasstation.backendPart1.service;
 
+import com.gasstation.backendPart1.dataTransferObject.UserDTO;
+import com.gasstation.backendPart1.model.User;
+import com.gasstation.backendPart1.repo.UserRepo;
+import jakarta.transaction.Transactional;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.modelmapper.ModelMapper;
+
+
+import java.util.List;
+
+@Service
+@Transactional
 public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<UserDTO> getAllUsers(){
+        List<User> userList=userRepo.findAll();
+        return modelMapper.map(userList,new TypeToken<List<UserDTO>>(){}.getType());
+
+    }
+    public UserDTO saveUser(UserDTO userDTO){
+        userRepo.save(modelMapper.map(userDTO,User.class));
+        return userDTO;
+    }
+
+    public UserDTO updateUser(UserDTO userDTO){
+        userRepo.save(modelMapper.map(userDTO,User.class));
+        return userDTO;
+    }
+
+    public UserDTO userDTO(UserDTO userDTO){
+        userRepo.save(modelMapper.map(userDTO,User.class));
+        return userDTO;
+    }
+
+    public String deleteUser(Integer userId){
+        userRepo.deleteById(userId);
+        return "deleted user successfully";
+    }
+
+
 }
