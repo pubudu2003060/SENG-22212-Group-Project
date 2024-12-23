6d1c457bae27998004fa9ed8e969f7b004d23011
rashmika <rashmikanethsarani119@gmail.com>
2024-12-24 00:21:15 +0530

    Implement UserController with CRUD operations for User entity

--- src/main/java/com/gasstation/backendPart1/BackendPart1Application.java
+++ src/main/java/com/gasstation/backendPart1/BackendPart1Application.java
@@ -1,7 +1,9 @@
 package com.gasstation.backendPart1;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendPart1Application {
@@ -9,5 +11,11 @@ public class BackendPart1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendPart1Application.class, args);
 	}
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 }
+
+
--- src/main/java/com/gasstation/backendPart1/controller/UserController.java
+++ src/main/java/com/gasstation/backendPart1/controller/UserController.java
@@ -1,6 +1,39 @@
 package com.gasstation.backendPart1.controller;
 
+import com.gasstation.backendPart1.dataTransferObject.UserDTO;
+import com.gasstation.backendPart1.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "api/v1/")
 public class UserController {
+
+    @Autowired
+
+    private UserService userService;
+
+    @GetMapping("/getusers")
+    public List<UserDTO> getUser(){
+        return userService.getAllUsers();
+    }
+    @PostMapping("/adduser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
+    }
+
+    @PutMapping("/updateuser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
+    }
+
+    @DeleteMapping("/deleteuser/{userId}")
+    public String deleteUser(@PathVariable Integer userId){
+        return userService.deleteUser(userId);
+    }
 
 
 
