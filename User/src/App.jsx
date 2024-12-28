055fd06697b7e4d1ed803543eac04d13ce430cfe
Rasadhi Sanchala <rspathirana1017@gmail.com>
2024-12-28 09:03:57 +0530

    Completed the Otp.jsx file functionalities.

--- User/src/App.jsx
+++ User/src/App.jsx
@@ -1,6 +1,4 @@
 
-
-import { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Login from './Components/LoginForm.jsx';
@@ -23,6 +21,6 @@ function App() {
   );
 }
 
-export default App;
+export default App;
 
 
