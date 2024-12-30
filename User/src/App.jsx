cd718fdebf82a21dfc28d37ba0c46c47a989c74f
Your Name <your-email@example.com>
2024-12-30 14:31:42 +0530

    Add HomePage route path

--- User/src/App.jsx
+++ User/src/App.jsx
@@ -1,9 +1,9 @@
 
-import {useState} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Login from './Components/LoginForm.jsx';
 import Otp from './Components/Otp.jsx';
+import HomePage from './Components/Home.jsx';
 
 function App() {
  
@@ -11,7 +11,8 @@ function App() {
     <Router> 
       <div>
         <Routes> 
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/LoginForm" element={<Login/>} /> 
           <Route path="/Otp" element={<Otp />} /> 
         </Routes>
       </div>
