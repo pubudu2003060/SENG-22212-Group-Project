d85bd15d79053bcc6fd3b186fa7b948a891c9a8c
Viduni-Hewage <viduhewage02@gmail.com>
2025-01-01 23:23:47 +0530

    modify names of the classes and ids unique (in dashboard)

--- User/src/Dashboard.jsx
+++ User/src/Dashboard.jsx
@@ -9,25 +9,39 @@ const vehicleNo = "";
 
 function Dashboard() {
     return ( 
-        <div>
+        <div className = "dashboard">
             <NavigationBar />
-            <div className = "body">
+            <div className = "dashboard-body">
             <div>
-                <h2>Welcome <span id = "name">{name}</span>....</h2>
-                <h3 id = "h3Text">Your Vehicles</h3>
-                <hr id = "longLine"/>
-                <div className = "vehicleInfo">
+                <h2 id = "dashboard-h2">Welcome <span id = "dashboard-name">{name}</span>....</h2>
+                <h3 id = "dashboard-h3Text">Your Vehicles</h3>
+                <hr id = "dashboard-longLine"/>
+                <div className = "dashboard-vehicleInfo">
                     <div>
-                        <p>Vehicle Type: {vehicleType}</p>
-                        <p>Fuel Balance: {fuelBalance}</p>
-                        <p>Eligible Days: {eligibleDays}</p>
+                        <p id = "dashboard-p">Vehicle Type: {vehicleType}</p>
+                        <p id = "dashboard-p">Fuel Balance: {fuelBalance}</p>
+                        <p id = "dashboard-p">Eligible Days: {eligibleDays}</p>
                     </div>
                     <div>
-                        <p>Vehicle No: {vehicleNo}</p>
-                        <p><a href = "">View</a> QR Code</p>
+                        <p id = "dashboard-p">Vehicle No: {vehicleNo}</p>
+                        <p id = "dashboard-p"><a href = "">View</a> QR Code</p>
                     </div>
                 </div>
-                <hr id = "smallLine"/>
+                <hr id = "dashboard-smallLine"/>
+                <div className = "dashboard-vehicleInfo">
+                    <div>
+                        <p id = "dashboard-p">Vehicle Type: {vehicleType}</p>
+                        <p id = "dashboard-p">Fuel Balance: {fuelBalance}</p>
+                        <p id = "dashboard-p">Eligible Days: {eligibleDays}</p>
+                    </div>
+                    <div>
+                        <p id = "dashboard-p">Vehicle No: {vehicleNo}</p>
+                        <p id = "dashboard-p"><a href = "">View</a> QR Code</p>
+                    </div>
+                </div>
+                <hr id = "dashboard-smallLine"/> 
+                               
+                
             </div>
 
         </div>
