afeb1c6d521eafddfe1fa1e84b90551faffddbfb
Viduni-Hewage <viduhewage02@gmail.com>
2025-01-01 15:23:47 +0530

    Add back home button with styling

--- User/src/PageNotFound.jsx
+++ User/src/PageNotFound.jsx
@@ -1,6 +1,9 @@
 import './PageNotFound.css';
+import { useNavigate } from "react-router-dom";
 
 function PageNotFound() {
+    const navigate = useNavigate();
+
     return (
         <div className="notFoundContainer">
             <div className="notFoundContent">
@@ -8,7 +11,10 @@ function PageNotFound() {
                 <h4 className="errorCode">404</h4>
                 <h5 className="error">Page Not Found</h5>
                 <p className="errorMessage">Oops... The page you are looking for does not exist.</p>
-                
+                <button className="backHome-btn" onClick={() => navigate("/dashboard")}>
+                    <img src="/images/arrow.png" alt="Go Back" id="arrow-img" />
+                    Go Back to Home
+                </button>
             </div>
         </div>
     );
