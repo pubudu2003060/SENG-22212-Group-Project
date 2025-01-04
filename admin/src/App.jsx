7d3c843955ecc1eb20459599b56340cd5c20709b
HiruniRamanayaka <146801854+HiruniRamanayaka@users.noreply.github.com>
2025-01-04 12:32:27 +0530

    I created navigation bar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Login';
-import Dashboard from './Dashboard';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Nav from "./components/Nav";
+import Dashboard from "./pages/Dashboard";
+import Navigation from "./pages/Navigation";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Navigation />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ admin/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../components/Nav";
+
+function Dashboard() {
+  return (
+    <>
+      <Nav />
+      <h1>Hiiii</h1>
+    </>
+  );
+}
+
+export default Dashboard;
