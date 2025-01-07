a49c72b78fd33012e273788689ef7a318c16e1c4
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-01-08 00:20:50 +0530

    add station owner class

---
+++ src/main/java/com/example/test/dto/FuelStationOwnerDTO.java
@@ -0,0 +1,15 @@
+package com.example.test.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class FuelStationOwnerDTO {
+
+    private int id;
+    private String name;
+    private String contact;
+}
