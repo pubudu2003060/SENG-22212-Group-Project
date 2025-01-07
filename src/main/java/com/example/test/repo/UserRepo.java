5924790270ee9b1e4f6138e5550d251447f846b7
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-01-07 13:21:24 +0530

    add user management

--- src/main/java/com/example/test/repo/UserRepo.java
+++ src/main/java/com/example/test/repo/UserRepo.java
@@ -1,4 +1,7 @@
 package com.example.test.repo;
 
-public class UserRepo {
+import com.example.test.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User,Integer> {
 }
