392d28dc588963e9ae54acca8415391c4b072fb4
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-01-07 23:19:57 +0530

    add admin controller

---
+++ src/main/java/com/example/test/model/Admin.java
@@ -0,0 +1,21 @@
+package com.example.test.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Admin {
+
+    @Id
+    private int adminId;
+    private String userName;
+    private String password;
+    private String email;
+
+}
