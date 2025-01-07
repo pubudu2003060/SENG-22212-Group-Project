7ae2505294cc67ebb04baca3c2629d59ed822745
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-01-07 23:19:57 +0530

    add admin controller

---
+++ src/main/java/com/example/test/repo/AdminRepo.java
@@ -0,0 +1,7 @@
+package com.example.test.repo;
+
+import com.example.test.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepo extends JpaRepository<Admin,Integer> {
+}
