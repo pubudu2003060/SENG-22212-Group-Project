97d80a4343f4efc8f4bf127f47e07ba8911d3eac
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-01-07 15:08:13 +0530

    add customer fual quata management

---
+++ src/main/java/com/example/test/controller/CustomerFuelQuotaController.java
@@ -0,0 +1,27 @@
+package com.example.test.controller;
+
+import com.example.test.dto.CustomerFuelQuotaDTO;
+import com.example.test.service.CustomerFualQuataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1")
+public class CustomerFuelQuotaController {
+
+    @Autowired
+    CustomerFualQuataService customerFualQuataService;
+
+    @GetMapping("/getcustomerquota")
+    public List<CustomerFuelQuotaDTO> getAllCustomerFuelQuotas() {
+        return customerFualQuataService.getAllCustomerFuelQuotas();
+    }
+
+    @PostMapping("/addcustomerquota")
+    public CustomerFuelQuotaDTO saveCustomerFuelQuota(@RequestBody CustomerFuelQuotaDTO customerFuelQuotaDTO) {
+        return customerFualQuataService.saveCustomerFuelQuota(customerFuelQuotaDTO);
+    }
+}
