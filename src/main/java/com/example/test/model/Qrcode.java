86d02dd7fe1df3a5864d1a954d641aca7eba3490
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-01-08 17:07:26 +0530

    change database

--- src/main/java/com/example/test/model/Qrcode.java
+++ src/main/java/com/example/test/model/Qrcode.java
@@ -2,6 +2,8 @@ package com.example.test.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,4 +17,8 @@ public class Qrcode {
     @Id
     private int qrcodeId;
     private int customerFualQuataId;
+
+    @OneToOne
+    @JoinColumn(name = "customerFuelQuotaId",referencedColumnName = "customerFuelQuotaId")
+    private CustomerFuelQuota customerFualQuata;
 }
