672c8b65a49a8bdfcd318eeff3e522616afcc41f
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-01-08 14:40:01 +0530

    add forign key contraints for buyquata table

--- src/main/java/com/example/test/dto/BuyQuotaDTO.java
+++ src/main/java/com/example/test/dto/BuyQuotaDTO.java
@@ -1,5 +1,10 @@
 package com.example.test.dto;
 
+import com.example.test.model.FuelStation;
+import com.example.test.model.User;
+import com.example.test.model.Vehical;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +19,16 @@ public class BuyQuotaDTO {
     private int bqId;
     private int amount;
     private Date date;
-    private int customerId;
-    private int vehicalId;
-    private int fuelstationId;
+
+    @ManyToOne
+    @JoinColumn(name = "customerId", referencedColumnName = "userId", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicalId", referencedColumnName = "vehicalId", nullable = false)
+    private Vehical vehical;
+
+    @ManyToOne
+    @JoinColumn(name = "fuelstationId", referencedColumnName = "stationId", nullable = false)
+    private FuelStation fuelStation;
 }
