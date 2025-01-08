673b10daf9b31c70923dc8abea1f1697d865d8b4
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-01-08 16:35:49 +0530

    change fualstation

--- src/main/java/com/example/test/model/FuelStation.java
+++ src/main/java/com/example/test/model/FuelStation.java
@@ -2,6 +2,8 @@ package com.example.test.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,4 +25,8 @@ public class FuelStation {
     private String fuelType;
     private String username;
     private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "stationOwnerid", referencedColumnName = "stationOwnerid", nullable = false)
+    private FuelStationOwner fuelStationOwner;
 }
