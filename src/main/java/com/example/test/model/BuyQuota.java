946c860e69e517ad9cf870b6eb4bc9e4564e1b56
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-01-08 14:52:45 +0530

    add forign key contraints for cfquata table

--- src/main/java/com/example/test/model/BuyQuota.java
+++ src/main/java/com/example/test/model/BuyQuota.java
@@ -1,9 +1,6 @@
 package com.example.test.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
