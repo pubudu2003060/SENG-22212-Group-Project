c70a87c14b508d2546ec8d868729a8a9f59d7eab
Viduni-Hewage <viduhewage02@gmail.com>
2025-01-12 19:05:03 +0530

    Modify the Header

--- User/src/WebHeader.jsx
+++ User/src/WebHeader.jsx
@@ -3,7 +3,12 @@ import './WebHeader.css';
 function WebHeader() {
     return (  
         <div className = "header">            
-            <img className="Comlogo" src="/images/logo.png" alt="ComLogo" />            
+            <img className="Comlogo" src="/images/logo.png" alt="ComLogo" />  
+            <div className = "header-info">
+                <p>passmyfuel@gmail.com</p>       
+                <p>+94 11 2369099</p>
+                <p>+94 11 2369099</p>
+            </div>       
         </div>
         
     );
