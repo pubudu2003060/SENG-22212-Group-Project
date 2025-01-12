06ca2fd886b43f32670409b23daceee1f4b27261
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-01-12 18:27:05 +0530

    resolve errors

---
+++ src/main/java/com/example/test/service/TwilioSmsService.java
@@ -0,0 +1,35 @@
+package com.example.test.service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class TwilioSmsService implements SmsService{
+    @Value("${twilio.accountSid}")
+    private String accountSid;
+
+    @Value("${twilio.authtoken}")
+    private String authToken;
+
+    @Value("${twilio.fromPhonenumber}")
+    private String fromPhoneNumber;
+
+    @PostConstruct
+    private void initTwilio() {
+        Twilio.init(accountSid, authToken);
+    }
+
+    @Override
+    public void sendOtp(String phoneNumber, String otp) {
+        Message.creator(
+                new com.twilio.type.PhoneNumber(phoneNumber),
+                new com.twilio.type.PhoneNumber(fromPhoneNumber),
+                "Your OTP is: "+otp
+        ).create();
+}
+}
