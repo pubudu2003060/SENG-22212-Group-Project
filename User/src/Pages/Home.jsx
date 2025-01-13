511c700aeb2d7ed52892007c89606b2d8829c450
Your Name <your-email@example.com>
2025-01-13 17:44:50 +0530

    added to the pages folder

---
+++ User/src/Pages/Home.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../Styles/Home.css";
+
+function HomePage() {
+    const navigate = useNavigate();
+    const [isRegistered, setIsRegistered] = useState(true); 
+
+    const goToLogin = () => {
+        navigate("/LoginForm");
+    };
+
+    const goToRegister = () => {
+        /*if (isRegistered) {
+            alert("You are already registered. Please log in instead.");
+        } else {
+            navigate("/PersonalDetailsForm");
+        }*/
+        navigate("/PersonalDetailsForm");
+    };
+
+    return (
+        <div className="home-page">
+            <div className="top-bar">
+               <div><img src="/images/lastfuel.png" alt="" /></div>
+               <div><h2>YOUR DIGITAL FUEL PASS MADE SIMPLE</h2></div>
+            </div>
+
+            <div className="text-container">
+                <div><h1 className="main-title">Welcome to the National Fuel Pass</h1></div>
+                <div><p className="subtitle">A platform for managing your fuel quota and distribution.</p></div>
+
+                <div className="buttons-container">
+                    <button onClick={goToRegister} className="btn-home">Register</button>
+                    <button onClick={goToLogin} className="btn-home">Login</button>
+                </div>
+
+                <div><p className="info-text">If you're a new user, please register first. If you're already registered, click Login.</p></div>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
