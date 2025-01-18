9506031f91e1e504edb50c26a976af696119c0aa
rashmika <rashmikanethsarani119@gmail.com>
2025-01-18 17:01:50 +0530

    make api for get all cfq

--- src/main/java/com/example/test/dto/CustomerFuelQuotaDTO.java
+++ src/main/java/com/example/test/dto/CustomerFuelQuotaDTO.java
@@ -5,26 +5,24 @@ import com.example.test.model.Vehical;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.Formula;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Getter
+@Setter
 public class CustomerFuelQuotaDTO {
-
 
     private int customerFuelQuotaId;
     private String eligibleDays;
     private int eligibleFuelQuota;
     private int remainFuel;
-
-    @ManyToOne
-    @JoinColumn(name = "customerId", referencedColumnName = "userId", nullable = false)
+    @Formula("eligible_fuel_quota - remain_fuel")
+    private int usedFuelQuota;
     private User user;
-
-    @OneToOne
-    @JoinColumn(name = "vehicalId", referencedColumnName = "vehicalId", nullable = false)
     private Vehical vehical;
+
+
 }
