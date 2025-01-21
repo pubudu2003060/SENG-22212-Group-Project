3656f146f190ab2b88b355fd5e764b4d957783f8
rashmika <rashmikanethsarani119@gmail.com>
2025-01-21 18:03:26 +0530

    created classes for homepage removed as some functionalities changed

--- src/main/java/com/example/test/repo/FuelStationWorkerHomepageRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.test.repo;
-
-import com.example.test.model.FuelStation;
-import com.example.test.model.UserLogin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuelStationWorkerHomepageRepo extends JpaRepository<FuelStation, Integer> {
-}
