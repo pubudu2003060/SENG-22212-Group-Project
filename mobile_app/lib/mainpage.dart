966743603916c20849672ad2d357638f8f97f3c1
Viduni-Hewage <viduhewage02@gmail.com>
2025-01-21 13:42:22 +0530

    Initialize and develop basics of mainpage

---
+++ mobile_app/lib/mainpage.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  void _showPopup(BuildContext context) {
+    Future.delayed(const Duration(seconds: 10), () {
+      if (!context.mounted) return;
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return Dialog(
+            backgroundColor: Color.fromARGB((0.9 * 255).toInt(), 255, 255, 255),
+
+
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Container(
+              margin: const EdgeInsets.all(10.0), 
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 20.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset(
+                        'assets/images/logo.png',
+                        width: 100, // Adjust the width as per your requirement
+                        height: 100, // Adjust the height as per your requirement
+                        fit: BoxFit.contain, // Maintain aspect ratio
+                      ),
+                      const SizedBox(height: 50),
+
+                      const Text(
+                        'Welcome',
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 18, 68, 109),
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 8),
+                      const Text(
+                        'to',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 18, 68, 109),
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 8),
+                      const Text(
+                        'PassMyFuel',
+                        style: TextStyle(
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 18, 68, 109),
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 180),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/login');
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 64, 146, 198),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 10),
+                        ),
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/register');
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 64, 146, 198),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 40, vertical: 10),
+                        ),
+                        child: const Text(
+                          'Register',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _showPopup(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Page'),
+        backgroundColor: const Color.fromARGB(255, 80, 171, 227),
+      ),
+      body: Center(
+        child: const Text(
+          'Main Screen Content',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+    );
+  }
+}
