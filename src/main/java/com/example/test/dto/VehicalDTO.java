a3442f68a9a75544946ec058ca81c4c0683c5d0c
rashmika <rashmikanethsarani119@gmail.com>
2025-01-21 17:53:44 +0530

    Created enum class for vehicle Type

--- src/main/java/com/example/test/dto/VehicalDTO.java
+++ src/main/java/com/example/test/dto/VehicalDTO.java
@@ -1,8 +1,8 @@
 package com.example.test.dto;
 
 import com.example.test.model.User;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import com.example.test.model.VehicalType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +14,7 @@ public class VehicalDTO {
 
     private int vehicalId;
     private int chassiNo;
-    private String vehicalType;
+    private VehicalType vehicalType;
     private int vehicalNo;
     private int enginNo;
     private String fualType;
