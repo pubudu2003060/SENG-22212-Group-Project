f21dcbab9184bfbcae0487f011ce91c89348e0d0
Viduni-Hewage <viduhewage02@gmail.com>
2025-01-21 09:00:33 +0530

    Add register page to the main.dart

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
+import 'register.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Fuel Station App',
       debugShowCheckedModeBanner: false, 
-      home: LoginScreen()
+      home: LoginScreen(),
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegisterScreen(),
+      },
 
       
     );
