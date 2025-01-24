176d390a8cc8e24bf4f9db80e0f0467374a73003
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-01-24 05:46:27 +0530

    REmoved dulicated imported

--- User/src/Components/VehicleDetailsForm.jsx
+++ User/src/Components/VehicleDetailsForm.jsx
@@ -1,8 +1,6 @@
 
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import React, {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom"; 
 import axios from "axios";
 import "../Styles/Registration.css";
 
