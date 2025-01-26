534b01fcd57395997a6c6a9593664b73cdfac8e9
rashmika <rashmikanethsarani119@gmail.com>
2025-01-27 01:19:55 +0530

    created findAdminBy usename method

--- src/main/java/com/example/test/repo/AdminRepo.java
+++ src/main/java/com/example/test/repo/AdminRepo.java
@@ -9,5 +9,7 @@ import java.util.List;
 public interface AdminRepo extends JpaRepository<Admin,Integer> {
 
     Admin getAdminByUserNameAndPassword(String userName, String password);
+    Admin findAdminByUserName(String userName);
+
 
 }
