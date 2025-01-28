315b042059f19cd7b181e2763433defc7e09293a
rashmika <rashmikanethsarani119@gmail.com>
2025-01-29 02:09:13 +0530

    encode the admin password when register

--- src/main/java/com/example/test/service/AdminService.java
+++ src/main/java/com/example/test/service/AdminService.java
@@ -4,8 +4,11 @@ import com.example.test.dto.AdminDTO;
 import com.example.test.dto.AdminSignInDTO;
 import com.example.test.dto.BuyQuotaDTO;
 import com.example.test.model.Admin;
+import com.example.test.model.AdminPrincipal;
 import com.example.test.model.BuyQuota;
 import com.example.test.repo.AdminRepo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
@@ -13,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,9 +25,13 @@ import java.util.List;
 @Transactional
 public class AdminService implements UserDetailsService {
 
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Autowired
     private AdminRepo adminrepo;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     @Autowired
     private ModelMapper modelMapper;
@@ -35,6 +43,7 @@ public class AdminService implements UserDetailsService {
     }
 
     public AdminDTO saveAdmin(AdminDTO adminDTO) {
+        adminDTO.setPassword(encoder.encode(adminDTO.getPassword()));
         adminrepo.save(modelMapper.map(adminDTO, Admin.class));
         return adminDTO;
     }
@@ -59,5 +68,6 @@ public class AdminService implements UserDetailsService {
             throw new UsernameNotFoundException("admin not found");
         }
 
+        return new AdminPrincipal(admin);
     }
 }
