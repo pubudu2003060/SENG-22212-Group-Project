84acf0b1559eab9b3e19d19b553ffa9b7c85a061
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-01-30 21:33:54 +0530

    add update function to cufuq

--- src/main/java/com/example/test/repo/CustomerFuelQuotaRepo.java
+++ src/main/java/com/example/test/repo/CustomerFuelQuotaRepo.java
@@ -5,6 +5,7 @@ import com.example.test.dto.VehicalFualQuataDTO;
 import com.example.test.model.CustomerFuelQuota;
 import com.example.test.enump.VehicalType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -31,13 +32,16 @@ public interface CustomerFuelQuotaRepo extends JpaRepository<CustomerFuelQuota, 
     @Query("SELECT new com.example.test.dto.QrCodeScanDetailsDTO( " +
             "u.firstName, u.lastName, u.idNo, " +
             "v.vehicalType, v.vehicalNo, v.fualType, " +
-           "c.eligibleDays, c.eligibleFuelQuota, c.remainFuel) " +
+            "c.eligibleDays, c.eligibleFuelQuota, c.remainFuel) " +
             "FROM CustomerFuelQuota c " +
             "JOIN c.user u " +
             "JOIN c.vehical v " +
             "WHERE c.customerFuelQuotaId = :customerFuelQuotaId")
     QrCodeScanDetailsDTO getQrCodeScanDetailsDTO(@Param("customerFuelQuotaId") int customerFuelQuotaId);
 
+    @Modifying
+    @Query("update CustomerFuelQuota c set c.remainFuel=:remainFuel where c.customerFuelQuotaId=:customerFuelQuotaId")
+    Object updateCustomerFueeldata(@Param("customerFuelQuotaId") int customerFuelQuotaId, @Param("remainFuel") int remainFuel);
 
 
 }
