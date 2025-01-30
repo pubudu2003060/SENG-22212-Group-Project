e77cc73ba3acd4d028c7292a04b021a2bd964fcf
rashmika <rashmikanethsarani119@gmail.com>
2025-01-30 14:02:08 +0530

    Iclude the role details to the token by identifying user type at the beginning

--- src/main/java/com/example/test/service/JWTService.java
+++ src/main/java/com/example/test/service/JWTService.java
@@ -1,5 +1,7 @@
 package com.example.test.service;
 
+import com.example.test.model.Admin;
+import com.example.test.repo.AdminRepo;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -20,10 +22,11 @@ import java.util.function.Function;
 
 @Service
 public class JWTService {
-   //nota safe way
+    private final AdminRepo adminRepo;
+    //nota safe way
     private String secretKey = "";
 
-    public JWTService() {
+    public JWTService(AdminRepo adminRepo) {
         //generate the key
         try {
             KeyGenerator keyGen=KeyGenerator.getInstance("HmacSHA256");
@@ -32,17 +35,26 @@ public class JWTService {
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
+        this.adminRepo = adminRepo;
     }
 
     public String generateToken(String userName) {
+        Admin admin = adminRepo.findAdminByUserName(userName);
+        String role="USER";//default role
+
+        if(admin != null){
+            role="ADMIN";//asign the role based on the admin
+        }
+
         //generate toke for every time that is logging
         Map<String, Object> claims = new HashMap<String, Object>();
+        claims.put("role", role);//include the role in the claim
         return Jwts.builder()
                 .setClaims(claims)
                 .addClaims(claims)
                 .setSubject(userName)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+60*60*30))
+                .setExpiration(new Date(System.currentTimeMillis()+60*60*30))//30min expiration
                 .signWith(getKey())//to sign in it needs the key
                 .compact();
 
@@ -58,12 +70,12 @@ public class JWTService {
         return extractClaims(token, Claims::getSubject);
     }
 
-    private <T> T extractClaims(String token, Function<Claims,T> claimResolver) {
+    public <T> T extractClaims(String token, Function<Claims, T> claimResolver) {
         final Claims claims=extractClaims(token);
         return claimResolver.apply(claims);
     }
 
-    private Claims extractClaims(String token) {
+    public Claims extractClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(getKey())
                 .build()
