f72f70b2a87c36aa1c5168d92fe7156c1a21bb82
rashmika <rashmikanethsarani119@gmail.com>
2025-02-01 00:17:30 +0530

    done some changes to avoid circular injection

--- src/main/java/com/example/test/Security/Services/FuelStationDetailsService.java
+++ src/main/java/com/example/test/Security/Services/FuelStationDetailsService.java
@@ -2,6 +2,7 @@ package com.example.test.Security.Services;
 
 import com.example.test.model.FuelStation;
 import com.example.test.repo.FuelStationRepo;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,8 +10,10 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.List;
 
+@Qualifier("fuelStationDetailsService")
 public class FuelStationDetailsService implements UserDetailsService {
     private final FuelStationRepo fuelStationRepo;
+
 
     public FuelStationDetailsService(FuelStationRepo fuelStationRepo) {
         this.fuelStationRepo = fuelStationRepo;
