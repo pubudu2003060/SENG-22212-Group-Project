eea30fcc97a907f82d3a674924d0c1d7c3d12a0e
Viduni-Hewage <viduhewage02@gmail.com>
2025-02-08 10:30:48 +0530

    Update the main .dart

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -7,6 +7,8 @@ import 'qr_scanner.dart';
 import 'qr_details.dart';
 import 'station_registration.dart';
 import 'pumpping_fuel_quata.dart';
+import 'dashboard.dart';
+import 'profile.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,29 +17,39 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Fuel Station App',
-      debugShowCheckedModeBanner: false,       
-       home: FirstScreen(),
-       routes: {
-         '/mainpage': (context) => MainScreen(),
-         '/login': (context) => LoginScreen(),
-         '/ownerregister': (context) => OwnerRegisterScreen(),
-         '/qr_scanner': (context) => QRScannerScreen(),
-        '/qr_details':(context) => DetailsScreen(firstName: '', lastName: '', idNo: '', vehicalType: '', vehicalNo: '', fualType: '', eligibleDays: '', eligibleFuelQuota: 0, remainFuel: 0, customerFuelQuotaId: 0,),
-         '/station_registration' : (context) => StationRegisterScreen(stationOwnerId:-1,),
-         '/pumpping_fuel_quata' : (context) => PumpingFuelQuota(
-           remainFuel: 0,
-           customerFuelQuotaId: 0,
-         ),
-    
-       },  
-
-      
-      
+      debugShowCheckedModeBanner: false,
+      home: FirstScreen(),
+      routes: {
+        '/mainpage': (context) => MainScreen(),
+        '/login': (context) => LoginScreen(),
+        '/ownerregister': (context) => OwnerRegisterScreen(),
+        '/qr_scanner': (context) => QRScannerScreen(),
+        '/qr_details': (context) => DetailsScreen(
+              firstName: '',
+              lastName: '',
+              idNo: '',
+              vehicalType: '',
+              vehicalNo: '',
+              fualType: '',
+              eligibleDays: '',
+              eligibleFuelQuota: 0,
+              remainFuel: 0,
+              customerFuelQuotaId: 0,
+            ),
+        '/station_registration': (context) => StationRegisterScreen(
+              stationOwnerId: -1,
+            ),
+        '/pumpping_fuel_quata': (context) => PumpingFuelQuota(
+              remainFuel: 0,
+              customerFuelQuotaId: 0,
+            ),
+        '/dashboard': (context) => DashboardPage(),
+        '/profile': (context) => ProfilePage()
+      },
     );
   }
 }
