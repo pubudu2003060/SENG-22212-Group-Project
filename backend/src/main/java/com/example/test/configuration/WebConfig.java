abebe25ed10c1bd5ac594a7e179698202456cea9
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-04-28 22:20:01 +0530

    addd a config for frontend

---
+++ backend/src/main/java/com/example/test/configuration/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.test.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://passmyfueluser.netlify.app/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
